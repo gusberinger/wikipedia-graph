428c0e0c94ea081009cd57c29b13955d44ce6c6e
Gus Beringer <gusberinger@gmail.com>
2023-12-07 20:21:06 -0600

    move processing line to own function

--- build_database/trim_links.py
+++ build_database/trim_links.py
@@ -1,4 +1,5 @@
 import gzip
+import itertools
 import pickle
 import re
 from tqdm import tqdm
@@ -6,33 +7,36 @@ from constants import LINKS_FILEPATH, LINKS_TRIMMED_FOLDER
 
 LINKS_REGEX = re.compile(r"(\d+),0,'(.+?)',0,(.*?)$")
 
-def main():
+def process_line(large_line: str) -> list[tuple[int, str, int]]:
+    if not large_line.startswith("INSERT INTO `pagelinks` VALUES ("):
+        return []
+    large_line = large_line.replace("INSERT INTO `pagelinks` VALUES (", "")
+    lines = large_line.replace("),(", "\n").splitlines()
+    batch = []
+    for line in lines:
+        if match := re.match(LINKS_REGEX, line):
+            line = line.replace(",NULL);", "NULL")
+            if not line.strip().endswith("NULL"):
+                print(f"{line=}")
+            from_id, title, target_id_match = match.groups()
+            target_id = None if "NULL" in target_id_match else int(target_id_match)
+            assert target_id is None
+            batch.append((int(from_id), title, target_id))
+    return batch
 
+def save_batch(batch: list[tuple[int, str, int]], batch_id: int):
+    with (LINKS_TRIMMED_FOLDER / f"{batch_id:04}.pickle").open("wb") as f:
+        pickle.dump(batch, f)
+
+def main():
     with gzip.open(LINKS_FILEPATH, "rt") as f:
-        links_batch: list[tuple[int, str, int]] = []
-        links_batch_id = 0
-        for large_line in tqdm(f, total=67856, desc="Trimming links file"):
-            if not large_line.startswith("INSERT INTO `pagelinks` VALUES ("):
-                continue
-            large_line = large_line.replace("INSERT INTO `pagelinks` VALUES (", "")
-            lines = large_line.replace("),(", "\n").splitlines()
-            for line in lines:
-                if match := re.match(LINKS_REGEX, line):
-                    line = line.replace(",NULL);", "NULL")
-                    if not line.strip().endswith("NULL"):
-                        print(f"{line=}")
-                    from_id, title, target_id_match = match.groups()
-                    target_id = None if "NULL" in target_id_match else int(target_id_match)
-                    links_batch.append((int(from_id), title, target_id))
-                    
-                    if len(links_batch) > 10 ** 6:
-                        with (LINKS_TRIMMED_FOLDER / f"{links_batch_id:04}.pickle").open("wb") as f:
-                            pickle.dump(links_batch, f)
-                        links_batch = []
-                        links_batch_id += 1
-                
-    with (LINKS_TRIMMED_FOLDER / f"{links_batch_id + 1:04}.pickle").open("wb") as f:
-        pickle.dump(links_batch, f)
+        chunk_size = 100
+        for i, lines in tqdm(enumerate(itertools.batched(f, chunk_size)), total=67856 // chunk_size, desc="Trimming links file"):
+            links = []
+            for large_line in lines:
+                links.extend(process_line(large_line))
+            save_batch(links, i)
+            
 
 if __name__ == "__main__":
     main()
