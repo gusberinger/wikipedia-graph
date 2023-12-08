2d54b5c45953b8780b5d8358dafedd4b149ef514
Gus Beringer <gusberinger@gmail.com>
2023-12-08 09:47:11 -0600

    remove extra variable in download script

--- build_database/download.py
+++ build_database/download.py
@@ -8,22 +8,25 @@ from constants import REDIRECTS_FILEPATH, LINKS_FILEPATH, PAGES_FILEPATH
 
 def main(*, replace_existing: bool = False):
     base_url = "https://dumps.wikimedia.org/enwiki/latest/"
-    files: list[tuple[Path, str]] = [
-        (REDIRECTS_FILEPATH, "enwiki-latest-redirect.sql.gz"),
-        (LINKS_FILEPATH, "enwiki-latest-pagelinks.sql.gz"),
-        (PAGES_FILEPATH, "enwiki-latest-page.sql.gz"),
+    files = [
+        REDIRECTS_FILEPATH,
+        PAGES_FILEPATH,
+        LINKS_FILEPATH,
     ]
-    for path, download_name in files:
+    for path in files:
         if not replace_existing and path.exists():
             continue
-        url = base_url + download_name
+        url = base_url + path.name
         response = requests.get(url, stream=True)
-        with tqdm(total=int(response.headers["Content-Length"])) as pbar:
-            pbar.set_description(f"Downloading {download_name}")
+        with tqdm(
+            total=int(response.headers["Content-Length"]),
+            desc=f"Downloading {path.name}",
+        ) as pbar:
             with path.open("wb") as f:
                 for chunk in response.iter_content(chunk_size=1024):
                     f.write(chunk)
                     pbar.update(len(chunk))
+
 
 if __name__ == "__main__":
     main()
